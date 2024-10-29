b87b48de01d9b1df2153de9e95496deff144012f
Uriel Acioli <uriel.acioli@gmail.com>
2024-10-29 01:53:52 -0300

    fix(lib.rs): better macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use mlua::prelude::*;
 use mlua::{Lua, LuaSerdeExt, MultiValue, Result, Value};
 use serde::Serialize;
-macro_rules! raise {
-    ($lua:expr, $msg:expr) => {
-        $lua.pack_multi((Value::Nil, $lua.to_value($msg)?))
+// lua multi/single value return
+macro_rules! ret {
+    // returns a single value
+    ($lua:expr, $val:expr) => {
+        $lua.pack_multi($lua.to_value($val)?)
+    };
+    // returns multiple values
+    ($lua:expr, $($val:expr),*) => {
+        $lua.pack_multi(($($lua.to_value($val)?),*))
     };
 }
 
@@ -12,12 +18,12 @@ fn decode<'lua>(lua: &'lua Lua, src: Value<'lua>) -> Result<MultiValue<'lua>> {
     match src {
         Value::String(ref ptr) => match ptr.to_str() {
             Ok(s) => match toml::from_str::<toml::Value>(s) {
-                Ok(v) => lua.pack_multi(lua.to_value(&v)),
-                Err(_e) => raise!(lua, "Error parsing toml string"),
+                Ok(v) => ret!(lua, &v),
+                Err(_e) => ret!(lua, "Error parsing toml string"),
             },
-            Err(_) => raise!(lua, "Error converting value to string"),
+            Err(_) => ret!(lua, "Error converting value to string"),
         },
-        _ => raise!(lua, "Error converting value to string"),
+        _ => ret!(lua, "Error converting value to string"),
     }
 }
 // returns either a value or nil and an error message
@@ -27,13 +33,13 @@ fn decode_file<'lua>(lua: &'lua Lua, src: Value<'lua>) -> Result<MultiValue<'lua
             Ok(path) => match std::fs::read_to_string(path) {
                 Ok(file) => match toml::from_str::<toml::Value>(&file) {
                     Ok(v) => lua.pack_multi(lua.to_value(&v)),
-                    Err(_e) => raise!(lua, "Error parsing toml file"),
+                    Err(_e) => ret!(lua, "Error parsing toml file"),
                 },
-                Err(_) => raise!(lua, "Error reading file"),
+                Err(_) => ret!(lua, "Error reading file"),
             },
-            Err(_) => raise!(lua, "Error converting value to string"),
+            Err(_) => ret!(lua, "Error converting value to string"),
         },
-        _ => raise!(lua, "Error converting value to string"),
+        _ => ret!(lua, "Error converting value to string"),
     }
 }
 
@@ -42,7 +48,7 @@ fn encode<'lua>(lua: &'lua Lua, v: Value<'lua>) -> Result<MultiValue<'lua>> {
     let mut buf = String::new();
     match v.serialize(toml::Serializer::new(&mut buf)) {
         Ok(()) => lua.pack_multi(lua.create_string(&buf).map(Value::String)),
-        Err(_) => raise!(lua, "Error serializing lua table to toml string"),
+        Err(_) => ret!(lua, "Error serializing lua table to toml string"),
     }
 }
 
